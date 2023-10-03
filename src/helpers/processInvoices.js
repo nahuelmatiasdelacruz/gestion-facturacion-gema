b2fd06c8f615e1675c15eb6fbf6d1d46168e8bf7
Nahuel De La Cruz <n_p4nic@hotmail.com>
2023-10-03 13:56:19 -0300

    Mejoras en la lectura de facturas y comunicación con MongoDB

--- src/helpers/processInvoices.js
+++ src/helpers/processInvoices.js
@@ -6,97 +6,45 @@ const failed_invoice = require("../models/db/Failed_inovice");
 
 const processInvoice = async (invoiceData) => {
     if(invoiceData.operationType === "insert"){
-        const fileName = invoiceData.fullDocument.origin;
-        const invoice = {
+        let fileName = invoiceData.fullDocument.origin;
+        let invoice = {
             ...invoiceData.fullDocument,
             cuit: invoiceData.fullDocument.cuit || ""
         }
-        console.log("Datos de la factura: ");
-        console.log(invoice);
-        const hasPrevFile = await checkIfHasDoc(fileName);
-        console.log("Nombre del archivo: " + fileName);
-        if(!hasPrevFile){
-            const origen = path.join(__dirname,"..","..","..","ops_uploads","GEMA",fileName);
-            console.log("Origen: ");
-            console.log(origen);
-            console.log("1- Verificando si existe el archivo en la carpeta ops_uploads/GEMA");
-            if(fs.existsSync(origen)){
-                console.log("2- El archivo existe, verificando si la IA envió los datos completos...");
-                if(hasFullData(invoice)){
-                    // Mover a procesados si tiene data completa y guardar en la base de facturas
-                    console.log("3- La IA ha enviado los datos completos, moviendo el archivo a facturas_procesadas");
-                    const clientFolder = path.join(__dirname,"..","..","..","facturas_procesadas",invoice.cuit.toString());
-                    console.log("Client Folder: ");
-                    console.log(clientFolder);
-                    const destFolder = path.join(__dirname,"..","..","..","facturas_procesadas",invoice.cuit.toString(),invoice.origin);
-                    console.log("Dest Folder: ");
-                    console.log(destFolder);
-                    if(!fs.existsSync(clientFolder)){
-                        try{
-                            fs.mkdirSync(clientFolder);
-                            console.log("Se ha creado la carpeta del cliente correctamente");
-                        }catch(e){
-                            console.log(e);
-                        }
-                    }
-                    try{
-                        fs.renameSync(origen,destFolder);
-                        const newInvoice = new Invoice({
-                            remito: invoice.remito,
-                            cuit: invoice.cuit,
-                            numeroBoca: invoice.boca,
-                            numeroImpreso: "",
-                            date: invoice.fecha,
-                            rendido: false,
-                            rutaArchivo: invoice.origin,
-                            checkId: "",
-                            clientManager: null,
-                        });
-                        await newInvoice.save();
-                    }catch(e){
-                        console.log("Hubo un error al mover el archivo");
-                        console.log(e);
-                    }
-                }else{
-                    // Mover a pendientes si le faltan datos y guardar en la base de pendientes
-                    console.log("3- La IA no ha completado todos los datos, moviendo el archivo a la carpeta: facturas_pendientes");
-                    try{
-                        const pendingFolder = path.join(__dirname,"..","..","..","facturas_pendientes",invoice.origin);
-                        fs.renameSync(origen,pendingFolder);
-                        const parsedInvoice = {
-                            ...invoice,
-                            cuit: isNaN(invoice.cuit) ? "" : invoice.cuit
-                        }
-                        const newFailedInvoice = new failed_invoice(parsedInvoice);
-                        await newFailedInvoice.save();
-                    }catch(e){
-                        console.log("Error al mover el archivo a carpeta de pendientes");
-                        console.log(e);
-                    }
+        fileName = await checkIfHasDoc(fileName);
+        invoice = {
+            ...invoice,
+            origin: fileName
+        }
+        const origen = path.join(__dirname,"..","..","..","ops_uploads","GEMA",fileName);
+        if(fs.existsSync(origen)){
+            try{
+                const pendingFolder = path.join(__dirname,"..","..","..","facturas_pendientes",invoice.origin);
+                fs.renameSync(origen,pendingFolder);
+                const parsedInvoice = {
+                    ...invoice,
+                    cuit: ""
                 }
-            }else{
-                console.log("2- El archivo no existe en la carpeta ops_uploads/GEMA. Se detiene el proceso");
+                const newFailedInvoice = new failed_invoice(parsedInvoice);
+                await newFailedInvoice.save();
+            }catch(e){
+                console.log("Error al mover el archivo a carpeta de pendientes");
+                console.log(e);
             }
+        }else{
+            console.log("2- El archivo no existe en la carpeta ops_uploads/GEMA. Se detiene el proceso");
         }
     }
 }
 const checkIfHasDoc = async (docName) => {
     const doc = await Facturasia.find({origin: docName});
-    if(doc.length > 1){
-        return true;
+    if(doc.length >= 1){
+        const origen = path.join(__dirname,"..","..","..","ops_uploads","GEMA",docName);
+        const newPath = path.join(__dirname,"..","..","..","ops_uploads","GEMA",`${Date.now()}.pdf`);
+        fs.renameSync(origen,newPath);
+        return `${Date.now()}.pdf`;
     }else{
-        return false;
-    }
-}
-const hasFullData = (doc) => {
-    console.log("Chequeando si tiene todos los datos: ");
-    console.log(doc);
-    if((doc.factura !== "" || doc.remito !== "") && (doc.cuit !== "" && !isNaN(doc.cuit))){
-        console.log("Tiene (factura o remito), CUIT y fecha");
-        return true;
-    }else{
-        console.log("Faltan datos");
-        return false;
+        return docName;
     }
 }
 
--- src/helpers/watchFolder.js
+++ src/helpers/watchFolder.js
@@ -1,31 +1,25 @@
-const fs = require('fs');
 const chokidar = require("chokidar");
 const path = require('path');
-const axios = require('axios');
 const Facturasia = require("../models/db/Facturasia");
 
 const folder = path.join(__dirname, "..","..","..","ops_uploads","GEMA");
-console.log(folder);
 const watchChangesFolder = () => {
-    const watcher = chokidar.watch(folder,{ignoreInitial: true});
+    const watcher = chokidar.watch(folder,{ignoreInitial: true, awaitWriteFinish: true});
     watcher.on("add",async (fileName)=>{
         const pdfName = fileName.split("/var/www/html/ops_uploads/GEMA/")[1];
-        console.log(`Se ha cargado el archivo: ${pdfName}`);
-        //axios.get("http://139.144.191.36/BIGEN Facturas/05.php?dest=GEMA");
         try{
             const newDocument = new Facturasia({
-                cuit: "",
-                factura: "",
-                remito: "",
-                boca: "",
-                fecha: "",
+                cuit: "no data",
+                factura: "no data",
+                remito: "no data",
+                boca: "no data",
+                fecha: "no data",
                 origin: pdfName
             });
             await newDocument.save();
         }catch(e){
             console.log(e);
         }
-        console.log("Solicitud a la IA enviada.");
     })
 }
 
